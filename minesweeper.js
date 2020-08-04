3c06ba783bc0bfa192818c314640f8edbe6a8a63
jongmin94 <jongmin94@kakao.com>
2020-08-04 17:07:58 +0900

    update : minesweeper.js
    1. 빈칸의 innerHTML 수정
    2. loopAroundCell 함수 추가 및 적용
    3. 좌&우클릭 동시 클릭 이벤트 리스너 추가

--- minesweeper.js
+++ minesweeper.js
@@ -77,7 +77,7 @@ const createNewGameBoard = difficulty => {
             cell.classList.add('cell');
 
             // fill with dummy data 
-            cell.innerHTML = `ㅁ`;
+            cell.innerHTML = `  `;
 
             // add event listener
             cell.addEventListener('click', cellClickEventListener);
@@ -121,6 +121,12 @@ const cellClickEventListener = event => {
     const cell = event.srcElement;
     const isMine = cell.classList.contains('mine');
 
+    // 0. If left and right button are clicked, call cellAllClickEventListerner
+    if(event.buttons === 2){
+        cellAllClickEventListener(event);
+        return;
+    }
+
     // 1. check this cell is mine
     if(isMine) {
         // game end
@@ -138,22 +144,10 @@ const cellClickEventListener = event => {
 
     let aroundMineCnt = 0;
 
-    for(let h = cellY - 1; h <= (cellY + 1); h++){
-        // check is valid heigth
-        if(h < 0 || h > height - 1) 
-            continue;
-
-        for(let w = cellX - 1; w <= (cellX + 1); w++){
-            // check is valid width
-            if(w < 0 || w > width - 1)
-                continue;
-            let aroundCell = document.getElementById(`w${w}h${h}`);
-            if(aroundCell.classList.contains('mine'))
-                aroundMineCnt++;
-        } // end w-for
-
-    } // end h-for
-    
+    loopAroundCell(cell, aroundCell => {
+        if(aroundCell.classList.contains('mine'))
+            aroundMineCnt++;
+    });
 
     // 3. Add clikced_n class to this cell's classList
     cell.classList.add(`clicked_${aroundMineCnt}`);
@@ -166,31 +160,18 @@ const cellClickEventListener = event => {
         //  4-2. If n is 0, loop around this cell and if that cell have not click_0, click them.
         cell.innerHTML = '';
         
-        for(let h = cellY - 1; h <= (cellY + 1); h++){
-            // check is valid heigth
-            if(h < 0 || h > height - 1) 
-                continue;
-    
-            for(let w = cellX - 1; w <= (cellX + 1); w++){
-                // check is valid width
-                if(w < 0 || w > width - 1)
-                    continue;
-
-                let aroundCell = document.getElementById(`w${w}h${h}`);
-
-                if(aroundCell.classList.contains(`clicked_0`))  
-                    continue;
+        loopAroundCell(cell, aroundCell => {
+            if(aroundCell.classList.contains(`clicked_0`))  
+                return;
                 
-                aroundCell.click();
-            } // end w-for
-        } // end h-for
+            aroundCell.click();
+        });
     }
-    
-
 }
 
 /**
  * cell's right click event listener 
+ * 
  * 1. check this cell is clicked. If this cell is already clicked, return
  * 2. check this cell's states(cell, flag, question)
  */
@@ -220,7 +201,7 @@ const cellContextMenuEventListener = event => {
     } else if(cell.classList.contains('question')) {
         // question -> cell
         cell.classList.remove('question');
-        cell.innerHTML ='ㅁ';
+        cell.innerHTML ='  ';
     } else {
         // cell -> flag 
         cell.innerHTML = 'F';
@@ -229,6 +210,129 @@ const cellContextMenuEventListener = event => {
 
 }
 
+/**
+ * cell's left & right click evnet listener
+ * 
+ * 1. check around cell and count mine and flag
+ * 2. If mine's count is not same flag's count (aroundMineCnt !== aroundFlagCnt), return
+ * 3. check flag is above mine or return
+ * 4. If all flag is correct position, click around not flag cell
+ */
+const cellAllClickEventListener = event => {
+    // 1. check around cell. 
+    // 주변 셀들의 깃발을 센다. 
+    // 깃발의 수 === 자신 주변의 마인 수일때 로직 진행
+
+    const cell = event.srcElement;
+    let isClicked = false;
+
+    let aroundMineCnt;
+    let aroundFlagCnt;
+
+    let isCorrect; 
+
+    for(let clazz of cell.classList){
+        if(clazz.startsWith('clicked')){
+            isClicked = true;
+        }
+    }
+
+    // 이미 클릭되어 밝혀진 셀에서만 로직을 진행한다. 
+    if(!isClicked)
+        return;
+
+    // 주변의 지뢰와 깃발의 개수를 센다. 
+    // 지뢰와 깃발의 개수가 같을 때만 로직을 진행한다. 
+    aroundMineCnt = 0;
+    aroundFlagCnt = 0;
+    
+    loopAroundCell(cell, aroundCell => {
+        for(let clazz of aroundCell.classList) {
+            if(clazz.startsWith('clicked')) 
+                return;
+        }
+        if(aroundCell.classList.contains('mine')) 
+            aroundMineCnt++;
+    
+        if(aroundCell.classList.contains('flag'))
+            aroundFlagCnt++;
+    });
+
+    // 지뢰와 깃발의 개수가 다르면 종료한다. 
+    if(aroundMineCnt !== aroundFlagCnt)
+        return;
+    
+
+    // 모든 깃발이 지뢰 위에 있는지 체크한다. 
+    // 지뢰 개수 === 깃발 개수이므로, 깃발은 반드시 지뢰 위에만 있어야 한다. 
+    // 즉, 지뢰인 곳에 깃발이 없으면 틀린 것이며, 
+    //     지뢰가 아닌 곳에 깃발이 있어도 틀린 것이다. 
+    isCorrect = true; 
+    
+    loopAroundCell(cell, aroundCell => {
+        for(let clazz of aroundCell.classList) {
+            if(clazz.startsWith('clicked')) 
+                // if this cell(around cell) is already clicked, continue w_loop
+                return;  
+        }
+
+        if(aroundCell.classList.contains('mine') && !aroundCell.classList.contains('flag')){
+            isCorrect = false;
+        } else if(!aroundCell.classList.contains('mine') && aroundCell.classList.contains('flag')){
+            isCorrect = false;
+        }
+    });
+
+
+    if(!isCorrect){
+        // 틀렸다면 게임을 종료시킨다. 
+        console.log('game over');
+        return;
+    } else {
+        // 정답인 경우 flag가 아닌 주변 셀을 클릭한다. 
+        loopAroundCell(cell, aroundCell => {
+            for(let clazz of aroundCell.classList) {
+                if(clazz.startsWith('clicked')) 
+                    return;   
+            }
+            
+            if(!aroundCell.classList.contains('flag'))
+                aroundCell.click();
+        })
+    }
+
+}
+
+/**
+ * loop around targetCell and call callback func
+ * 
+ * @param {*} targetCell center cell
+ * @param {*} callback callback
+ */
+const loopAroundCell = (targetCell, callback) => {
+    const width = widths[difficulty.value];
+    const height = heights[difficulty.value];
+    
+    const cellX = Number(targetCell.width);
+    const cellY = Number(targetCell.height);
+    
+    for(let h = cellY - 1; h <= (cellY + 1); h++){
+        // check is valid heigth
+        if(h < 0 || h > height - 1) 
+            continue;
+
+        for(let w = cellX - 1; w <= (cellX + 1); w++){
+            // check is valid width
+            if(w < 0 || w > width - 1)
+                continue;
+
+            let aroundCell = document.getElementById(`w${w}h${h}`);
+
+            callback(aroundCell);
+        } // end w_loop
+    } // end h_loop
+}
+ 
 window.onload = () => {
     /* initialize start*/
     let newGame = createNewGame(difficulty.value);
